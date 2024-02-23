a8c3a25eeb9d39ec9046e0273e85688cff6d278c
PioRomo <promocolleges@gmail.com>
2024-02-23 09:32:05 -0800

    Home page, login page set up. Added routes

---
+++ spartanflea/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css';
+import {Route, Routes} from "react-router-dom"; 
+import LoginPage from './pages/LoginPage';
+import HomePage from './pages/HomePage'
+
+function App() {
+  return (
+    <Routes>
+      <Route index element ={<HomePage />} />
+        <Route path ="/login" element={<LoginPage />} />
+    </Routes>
+  )
+}
+
+export default App;
---
+++ spartanflea/src/pages/HomePage.jsx
@@ -0,0 +1,10 @@
+import {Link} from "react-router-dom";
+
+export default function LoginPage(){
+
+    return(
+        <div className ="bg-red-600">
+            <h1 className="text-black" >Hello!</h1>
+        </div>
+    );
+}
