ed0f54ce2ae303c8a2c834acf9609f0e9d23b0e2
PioRomo <promocolleges@gmail.com>
2024-02-28 18:07:44 -0800

    Home Page template, blank registration page added

--- spartanflea/src/App.jsx
+++ spartanflea/src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import {Route, Routes} from "react-router-dom";
 import IndexPage from './pages/IndexPage'
-
+import RegistrationPage from './pages/RegistrationPage'; 
 function App(){
   return(
     <Routes>
       <Route index element={<IndexPage/>}/>
+        <Route path="/register" element={<RegistrationPage />} />
+        
     </Routes>
   );
 
--- spartanflea/src/pages/IndexPage.jsx
+++ spartanflea/src/pages/IndexPage.jsx
@@ -1,15 +1,27 @@
-export default function HomePage(){
+import {Link} from "react-router-dom";
+import RegistrationPage from "./RegistrationPage";
+
+export default function IndexPage(){
 
     return(
-        <div className="">
-            <div className="">
-                <img className = "" src ="https://docs.google.com/drawings/d/e/2PACX-1vSoyGWRLYwYovMlLkiEwC9D3iWQDKVmiXFt_80xsv0721UmTsETuE1j2tsbpcqxrTsZ5EN9CDQzCuZo/pub?w=4320&h=864" alt="SpartanFlea wordmark"></img>
+        <body className="bg-yellow-200">
+            {/*This div controls the header */}
+            <div className=" bg-blue-200 flex">
+                <a href="/" className="">
+                    <img className = "w-1/3" src ="https://docs.google.com/drawings/d/e/2PACX-1vSoyGWRLYwYovMlLkiEwC9D3iWQDKVmiXFt_80xsv0721UmTsETuE1j2tsbpcqxrTsZ5EN9CDQzCuZo/pub?w=4320&h=864" alt="SpartanFlea wordmark"></img>
+                </a>
+
+                <Link to={'/register'}>
+                    Hello
+                </Link>
             </div>
 
-            <div className ="text-xl">
-                Coming soon!
+            <div className="h-screen w-screen">
+                
+                
             </div>
 
-        </div>
+        </body>
+        
     );
 }
